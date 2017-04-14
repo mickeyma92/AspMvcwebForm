365888bc205f63afba2d004ec081123572aa3726
mickeyma92 <microsoft1>
2017-04-14 18:48:10 -0500

    Modelo de prueba sin conexion db

---
+++ WebFormsAsp/WebFormsAsp/Models/PersonasModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFormsAsp.Models
+{
+    public class PersonasModel
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+    }
+}
