c3cf5beab05f79f2c2f7be0c8447743e4dc06c52
Miguel <micro_soft1@outlook.com>
2017-11-06 23:31:26 -0600

    Create ngclass.component.ts

---
+++ ngclass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejdirecticangclass',
+  templateUrl: './ejdirecticangclass.component.html',
+  styleUrls: ['./ejdirecticangclass.component.css']
+})
+export class EjdirecticangclassComponent implements OnInit {
+  puntuacion:number;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
